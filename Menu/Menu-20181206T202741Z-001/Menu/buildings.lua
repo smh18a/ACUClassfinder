e95ea4ff0cc8bac148bdc7d20732e3ce85ee15a3
smh18a <42848794+smh18a@users.noreply.github.com>
2018-12-06 14:36:40 -0600

    Add files via upload

---
+++ Menu/Menu-20181206T202741Z-001/Menu/buildings.lua
@@ -0,0 +1,21 @@
+local buildings = {
+	{	
+		name = "Mabee Buisness Building",
+		thumbnail = "images/buildings/coba.png",
+		image = "images/buildings/coba.png",
+		description = "Buissness and IT"
+	},{
+		name = "Bennet Gymnasium",
+		thumbnail = "images/buildings/BennetGym.png",
+		image = "images/buildings/BennetGym.png",
+		description = "Enginnering labs i think lol"
+	},{
+		name = "Name of Building",
+		thumbnail = "directory of picture (dont worry about these yet)",
+		image = "another image (dont worry about it yet)",
+		description = "Desciption of Building (somthing short if you know)"
+
+}
+}
+
+return buildings
