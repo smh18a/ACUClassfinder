36a4b0222f31d0a6525d50fcec0e6f99f49a538a
smh18a <42848794+smh18a@users.noreply.github.com>
2018-12-08 21:19:44 -0600

    Add files via upload

---
+++ Codes/Final App-20181209/Final App-20181209T031515Z-001/Final App/Final Classfinder/building.lua
@@ -0,0 +1,204 @@
+local buildings = {
+--badirs code
+
+	{
+		name = "A.B. Barret Hall",
+		thumbnail = "images/buildings/hall.png",
+		image = "images/buildings/barretH.png",
+		description = "Dorm"
+
+	},{		
+		name = "A.B. Morris Hall",
+		thumbnail = "images/buildings/Morris.png",
+		image = "images/buildings/morrisH.png",
+		description = "Dorm"
+
+	},{
+		name = "Brown Library",
+		thumbnail = "images/buildings/brown.png",
+		image = "images/buildings/libraryH.png",
+		description = "Learning Center, Team 55 Helpdesk, CopyCat"
+
+	},{
+		name = "Bennet Gymnasium",
+		thumbnail = "images/buildings/BennetGym.png",
+		image = "images/buildings/bennetH.png",
+		description = "Enginnering labs"
+
+	},{
+		name = "Chapel on the Hill",
+		thumbnail = "images/buildings/chapel.png",
+		image = "images/buildings/chapelonhillH.png",
+		description = "Outdoor Auditorium"
+
+	},{
+		name = "Crutcher Scott Field",
+		thumbnail = "images/buildings/CrutcherScottField.png",
+		image = "images/buildings/crutcherH.png",
+		description = "Baseball Field"
+
+	},{
+		name = "Cullen Auditorium",
+		thumbnail = "images/buildings/culle.png",
+		image = "images/buildings/cullenH.png",
+		description = "Auditorium, Cornstone spotlight"
+
+	},{
+		name = "Edwards Hall",
+		thumbnail = "images/buildings/EdwardsHall.png",
+		image = "images/buildings/edwardsH.png",
+		description = "Sophmore Boys Dorm"
+
+	},{
+		name = "Elmer Gray Stadium",
+		thumbnail = "images/buildings/South.png",
+		image = "images/buildings/elmerH.png",
+		description = "Football Stadium"
+
+	},{
+		name = "Gardner Hall",
+		thumbnail = "images/buildings/Gardner.png",
+		image = "images/buildings/GardnerH.png",
+		description = "Freshman Girls Dorm"
+
+	},{
+		name = "Hardin Administration Building",
+		thumbnail = "images/buildings/adbuiliding.png",
+		image = "images/buildings/adminH.png",
+		description = "English Classes and Administrative offices"
+
+	},{
+		name = "Hunter Welcome Center",
+		thumbnail = "images/buildings/Hunter.png",
+		image = "images/buildings/hunterH.png",
+		description = "Administrative offices and lecture halls"
+
+	},{	
+		name = "Mabee Buisness Building",
+		thumbnail = "images/buildings/mabe1.png",
+		image = "images/buildings/CobaH.png",
+		description = "Buissness and IT"
+
+	},{
+		name = "Mabee Hall",
+		thumbnail = "images/buildings/eng.png",
+		image = "images/buildings/mabeeH.png",
+		description = "Freshman boys dorm"
+
+	},{
+		name = "McGlothin Campus Center(The Bean)",
+		thumbnail = "images/buildings/McGlothlinCampuscenter.png",
+		image = "images/buildings/BeanH.png",
+		description = "The Bean, ACU Mail, Campus Store, Advising Department"
+
+	},{
+		name = "McKinzie Hall",
+		thumbnail = "images/buildings/mckinz.png",
+		image = "images/buildings/mckinzieH.png",
+		description = "Freshman Boys Dorm"
+
+	},{
+		name = "Medical and Counseling Care Center",
+		thumbnail = "images/buildings/MedicalandCounselingCareCenter.png",
+		image = "images/buildings/medicalH.png",
+		description = "Student Clinic"
+
+	},{
+		name = "Moody Coliseum",
+		thumbnail = "images/buildings/moody.png",
+		image = "images/buildings/MoodyColiseumH.png",
+		description = "Basketball stadium, Chapel Auditorium"
+
+	},{
+		name = "Nelson Hall",
+		thumbnail = "images/buildings/nelsonhall.png",
+		image = "images/buildings/NelsonHallH.png",
+		description = "Freshman Girls Dorm"
+
+	},{
+		name = "Onstead-Packer Biblical Studies Building",
+		thumbnail = "images/buildings/biblebuilding.png",
+		image = "images/buildings/bibleH.png",
+		description = "Bible Lecture Halls"
+
+	},{
+		name = "Onstead Science Center",
+		thumbnail = "images/buildings/on.png",
+		image = "images/buildings/onsteadH.png",
+		description = "Sciences Classrooms, Math, Bio..."
+
+	},{
+		name = "Phillips Education Building",
+		thumbnail = "images/buildings/Phillips.png",
+		image = "images/buildings/PhillipsEducationBuildingH.png",
+		description = "Early Education, and Teaching"
+
+	},{
+		name = "Police Department",
+		thumbnail = "images/buildings/police.png",
+		image = "images/buildings/policeH.png",
+		description = "ACU PoliceDepartment \nIn case of emrgency  (325)-674-2911"
+
+	},{
+		name = "Student Recreation and Wellness Center",
+		thumbnail = "images/buildings/StudentRecreationandWellnessCenter.png",
+		image = "images/buildings/StudentRecH.png",
+		description = "Basketball Courts, Pools, Rawuet Ball, Weights"
+
+	},{
+		name = "Sherrod Building",
+		thumbnail = "images/buildings/Sherrod.png",
+		image = "images/buildings/SherrodBuildingH.png",
+		description = "Art Building"
+
+	},{
+		name = "Sikes Hall",
+		thumbnail = "images/buildings/skieshall.png",
+		image = "images/buildings/SkiesHallH.png",
+		description = "Freshman Girls Dorm"
+
+	},{
+		name = "Teague Center",
+		thumbnail = "images/buildings/TeagueCenter.png",
+		image = "images/buildings/TeagueCenterH.png",
+		description = "Tennis Building"
+
+	},{
+		name = "Sewell Theatre",
+		thumbnail = "images/buildings/acusewell.png",
+		image = "images/buildings/SewellTheatreH.png",
+		description = "Theare Building"
+
+	},{
+		name = "Universriy Park Apartments",
+		thumbnail = "images/buildings/UniversityParkApts.png",
+		image = "images/buildings/ParkAptH.png",
+		description = "Jr. Sr. on Campus Apts."
+
+	},{
+		name = "Wally Bullington Football Practice Facility",
+		thumbnail = "images/buildings/BennetGym.png",
+		image = "images/buildings/wallyH.png",
+		description = "Practice Football Field"
+
+	},{
+		name = "Wells Field",
+		thumbnail = "images/buildings/WellsField.png",
+		image = "images/buildings/WellsFieldH.png",
+		description = "Rec. Field"
+
+	},{
+		name = "Williams Performing Arts Center",
+		thumbnail = "images/buildings/art.png",
+		image = "images/buildings/WPACH.png",
+		description = "Theatre Building"
+
+	},{
+		name = "Zellner Hall",
+		thumbnail = "images/buildings/zellner.png",
+		image = "images/buildings/ZellnerH.png",
+		description = "IT offices, Networking and Hardware HQ"
+	},
+}
+
+return buildings
